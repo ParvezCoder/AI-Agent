cbc1e358324206da545004320cdca704f29b785b
Parvez Coder <ParvezCoder786@gmail.com>
2025-06-26 09:18:26 +0500

    dep

--- streamlit.py
+++ streamlit.py
@@ -7,8 +7,6 @@ from agents import Agent, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
 # Load .env key
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
-if not gemini_api_key:
-    raise ValueError("GEMINI_API_KEY not found in .env")
 
 # OpenAI-like Client
 external_client = AsyncOpenAI(
